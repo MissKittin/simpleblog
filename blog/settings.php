91985b3b051ecf78221c2c77e6eaca7dbb529260
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-10-18 22:35:18 +0200

    added taglink switch

--- blog/settings.php
+++ blog/settings.php
@@ -1,8 +1,7 @@
 <?php
-
-$cms_root='/blog/';
-$page_title='Blog';
-$entries_per_page=10;
-$skin='default'
-
+	$cms_root='/blog/'; // directory
+	$page_title='Blog'; // <title>
+	$entries_per_page=10;
+	$taglinks=true; // enable/disable tag as link
+	$skin='default'; // skin name
 ?>
