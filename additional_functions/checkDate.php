dc80b9c60b75139ef005ad90407a183899ef519f
MissKittin <56042452+MissKittin@users.noreply.github.com>
2019-11-08 16:16:31 +0100

    comments

--- additional_functions/checkDate.php
+++ additional_functions/checkDate.php
@@ -5,20 +5,23 @@
 		// example: simpleblog_checkDate('01.02', '25.06')
 
 		$start=explode('.', $start); $end=explode('.', $end);
-		$today=date('Y-m-d'); $today=date('Y-m-d', strtotime($today));
-		$thisYear=date('Y');
+		$today=date('Y-m-d'); $today=date('Y-m-d', strtotime($today)); // get time and convert it to unix timestamp
+		$thisYear=date('Y'); // cache year
 
 		if($start[1]<$end[1])
 		{
+			// start and end date is in one year
 			$startDate=date('Y-m-d', strtotime($start[1].'/'.$start[0].'/'.$thisYear));
 			$endDate=date('Y-m-d', strtotime($end[1].'/'.$end[0].'/'.$thisYear));
 		}
 		else
 		{
+			// both dates are at the turn of the years
 			if(($today<=date('Y-m-d', strtotime('12/31/'.$thisYear))) && (date('m')>11))
-				$newYear=$thisYear+1;
+				$newYear=$thisYear+1; // now is old year
 			else
 			{
+				// now is new year
 				$newYear=$thisYear;
 				$thisYear=$thisYear-1;
 			}
